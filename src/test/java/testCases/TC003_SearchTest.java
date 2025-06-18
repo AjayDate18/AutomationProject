089e5749c0289cf477d85af54b5c5dbe4a1116d2
AjayDate18 <116094923+AjayDate18@users.noreply.github.com>
2025-06-18 12:51:43 +0530

    Update TC003_SearchTest.java

--- src/test/java/testCases/TC003_SearchTest.java
+++ src/test/java/testCases/TC003_SearchTest.java
@@ -1,12 +1,12 @@
-package testCases;
+//package testCases;
 
-public class TC003_SearchTest 
-{
+//public class TC003_SearchTest 
+//{
 	//This Is Search Test In OpenCart
-	@Test
-	public void searchProduct()
-	{
-		//Search Logic
-	}
+//	@Test
+	//public void searchProduct()
+//	{
+//		//Search Logic
+//	}
 	
-}
+//}
